89a4659f3bf1bd409ba3f43d8544462c3bc80490
stevelouis4848 <stevelouis4848@github.com>
2018-05-31 11:14:38 -0400

    work on VM

--- VM.c
+++ VM.c
@@ -1,12 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int MAX_STACK_HEIGHT = 2000;
+int MAX_CODE_LENGTH = 500;
+int MAX_LEXI_LEVELS = 3;
+
+char *opCode[] = {"NULL", "lit", "lod", "sto", "cal", "inc", "jmp",
+                    "jpc", "sio", "neg", "add", "sub", "mul", "div", "odd",
+                    "mod", "eql", "neq", "lss", "leq", "gtr", "geq"};
 typedef struct instruction{
                  int op; // opcode
                  int r; // reg
                  int l; // L
                  int m; // M
                 }instruction;
+
+typedef struct enviroment{
+                 int pc; // opcode
+                 int bp; // reg
+                 int sp; // L
+                 instruction ir; // M
+               }enviroment;
 
 main(){
     for(i=1; i < argc; i++)
@@ -16,38 +30,56 @@ main(){
     return 0;
 }
 
-fetch(char* fileName){
+int base(l, base) // l stand for L in the instruction format
+{
+  int b1; //find base L levels down
+  b1 = base;   
 
-    instruction *newInstruction;
-    int* buff;
-    FILE *fp;
-
-    fp = fopen("fileName","r");
-
-
-    while( fscanf(fp, "%s", buff) != EOF ){
-
-        newInstruction.op = buff;
-
-        fscanf(fp, "%d", buff);
-        newInstruction.r = buff;
-
-        fscanf(fp, "%d", buff);
-        newInstruction.l = buff;
-
-        fscanf(fp, "%d", buff);
-        newInstruction.m = buff;
-
-        execute(newInstruction);
-        operations();
-        PCUpdate();
-    }
-    fclose(fp);
+  while (l > 0){
+    b1 = stack[b1 + 1];
+    l- -;
+  }
+    return b1;
 }
 
-execute(instruction *newInstruction){
+fetch(char* fileName){
 
-switch (newInstruction.op) {
+enviroment *env;
+int *stack;
+
+
+    FILE *ifp;
+    FILE *ofp;
+
+    ifp = fopen("fileName","r");
+    ofp =fopen("factOpPrint","w");
+    ofp2 =
+
+    for (i=0;i<MAX_STACK_HEIGHT;i++){
+
+          stack[i] = malloc(sizeof(int));
+          stack[i] = 0;
+        }
+
+
+    while( fscanf(ifp, "%d,%d,%d",env->ir.op,env->ir.r,env->ir.l
+                  env->ir.m)) != EOF ){
+
+      fprintf(ofp,"%s %d %d %d",opCode[env->ir.op],env->ir.r,env->ir.l,
+              env->ir.m);
+      execute(env);
+    }
+    fclose(ifp);
+    fclose(ofp);
+}
+
+execute(enviroment *env){
+
+  if(env->ir.op > 10){
+      OPR(env);
+      }
+
+switch (env->ir.op) {
     case 01:
         break;
     case 02:
@@ -68,6 +100,13 @@ switch (newInstruction.op) {
         break;
     case 10:
         break;
+  }
+
+}
+
+OPR(){
+
+  switch(env->ir.op){
     case 11:
         break;
     case 12:
@@ -93,28 +132,4 @@ switch (newInstruction.op) {
     case 22:
         break;
     }
-}
-
-operations(){
-
-}
-
-writeMemory(){
-
-}
-
-readMemory(){
-
-}
-
-readRegister(){
-
-}
-
-pcUpdate(){
-
-}
-
-Stack(){
-
 }
