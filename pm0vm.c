132135ef0441c019d3082514595fbb8cb8c758c2
st105227 <st105227@eustis2>
2018-06-04 20:41:58 -0400

    work

--- pm0vm.c
+++ pm0vm.c
@@ -25,10 +25,12 @@ int MAX_STACK_HEIGHT = 2000;
 int MAX_CODE_LENGTH = 500;
 int MAX_LEXI_LEVELS = 3;
 
+// Array of strings with the indexes corresponding to the name of the op codes.
 char *opCode[] = {"NULL", "LIT", "RTN", "LOD", "STO", "CAL", "INC", "JMP","JPC", "SIO",
 					"NEG", "ADD", "SUB", "MUL", "DIV", "ODD","MOD", "EQL", "NEQ",
 					"LSS", "LEQ", "GTR", "GEQ"};
 
+// Struct named instructions that holds the  the content of the instructions
 typedef struct instruction{
 							int op; // opcode
 							int r; // reg
@@ -36,6 +38,7 @@ typedef struct instruction{
 							int m; // M
 						}instruction;
 
+// Struct named enviroment that holds the information relevant to the instruction being currently execute.
 typedef struct enviroment{
 							int pc; // pc
 							int pcPrev; //previous pc
@@ -45,7 +48,8 @@ typedef struct enviroment{
 							instruction ir; // current instruction
 							
 					}enviroment;
-					
+
+// Function Prototypes for the program.					
 int base(int l, int base,int *stack);
 void vm (char* fileName);
 void fetch(int count, enviroment *env, instruction *irList, FILE *ofp);
@@ -82,6 +86,7 @@ void vm (char *fileName){
 	env->pc = 0;
 	*halt = 0;
 	
+	// Initializing file pointers to read the input file and files to output.
 	ifp = fopen(fileName, "r");
 	ofp = fopen("factOpPrint.txt", "w");
 	ofp2 = fopen("stackTracePrint.txt", "w");
@@ -94,11 +99,14 @@ void vm (char *fileName){
 		return;
 	}
 
+	// Prints the headers to the files.
 	fprintf(ofp,"\nFactorial Op Printout:\n");
 	fprintf(ofp2,"\nFactorial Stack Trace:\nPC OP Rg Lx Vl [PC BP SP]\n");
 	fprintf(ofp3,"\nFactorial Output:\n");
 	
 	count = 0;
+	
+	// Reads the input file one instruction at a time and stores it the array of instructions. 
 	while( fscanf(ifp, "%d %d %d %d",&buff[0],
 					&buff[1],&buff[2],&buff[3]) != EOF){
 
@@ -111,6 +119,8 @@ void vm (char *fileName){
 	}
 
 	count = 0;
+	
+	// Runs fetch then execute,then prints the calls printStack contents into 3 files.
 	while (*halt != 1 ){
 		
 		fetch(count, env, irList, ofp);
@@ -120,31 +130,38 @@ void vm (char *fileName){
 		printStack(3,env,env->sp,env->bp,stack,env->ir.l,ofp2);
 		count++;
 	}
+	
+	// Closes the files since we are done writing to them.
 	fclose(ifp);
 	fclose(ofp);
 	fclose(ofp2);
 	fclose(ofp3);
 	
+	// Initializing file pointers to be read and printed to the screen.
 	ofp = fopen("factOpPrint.txt", "r");
 	ofp2 = fopen("stackTracePrint.txt", "r");
 	ofp3 = fopen("factPrint.txt", "r");
 	
+	// Gets the contents of the 3 output files and prints them to the screen.
 	ofpPrint = fgetc(ofp);
 	while(ofpPrint != EOF){
 		printf("%c",ofpPrint);
 		ofpPrint = fgetc(ofp);		
 	}
+	
 	ofpPrint = fgetc(ofp2);
 	while(ofpPrint != EOF){
 		printf("%c",ofpPrint);
 		ofpPrint = fgetc(ofp2);		
 	}
+	
 	ofpPrint = fgetc(ofp3);
 	while(ofpPrint != EOF){
 		printf("%c",ofpPrint);
 		ofpPrint = fgetc(ofp3);		
 	}
 	
+	// Closes the file pointers since we are done printing them to the screen.
 	fclose(ofp);
 	fclose(ofp2);
 	fclose(ofp3);
@@ -153,18 +170,24 @@ void vm (char *fileName){
 
 void fetch(int count,enviroment *env, instruction *irList, FILE *ofp){
 	
+	// Takes the instruction code corresponding to the pc and puts it in the environment struct.
 	env->ir = irList[env->pc];
-					
+			
+	// Prints the contents of the current instruction a file.
 	fprintf(ofp,"%d %s %d %d %d \n",count,opCode[env->ir.op],env->ir.r,env->ir.l,
 					env->ir.m);
 	
+	// Stores the pc before it is incremented.
 	env->pcPrev = env->pc;
+	
+	// PC is incremented to be able to detch the next instruction.
 	env->pc++;
 	
 }
 
 void execute(enviroment *env,int *stack, int *halt,FILE *ofp, FILE *ofp2){
 
+	// Takes care of the math operations and sends them to a another function.
 	if(env->ir.op >= 10){
 		opr(env,stack);
 		return;
@@ -186,10 +209,10 @@ void execute(enviroment *env,int *stack, int *halt,FILE *ofp, FILE *ofp2){
 			stack[base(env->ir.l, env->bp, stack) + env->ir.m] = env->R[env->ir.r];
 			break;
 		case 5: //CAL
-			stack[env->sp + 1] = 0; //env->space to return value
-			stack[env->sp + 2] = base(env->ir.l, env->bp, stack); //static link (SL)
-			stack[env->sp + 3] = env->bp; //dynamic link (DL)
-			stack[env->sp + 4] = env->pc; //return address (RA)
+			stack[env->sp + 1] = 0;
+			stack[env->sp + 2] = base(env->ir.l, env->bp, stack);
+			stack[env->sp + 3] = env->bp; 
+			stack[env->sp + 4] = env->pc; 
 			env->bp = env->sp + 1;
 			env->pc = env->ir.m;	
 			env->sp = env->sp + 4;
@@ -228,6 +251,7 @@ void execute(enviroment *env,int *stack, int *halt,FILE *ofp, FILE *ofp2){
 
 void opr(enviroment *env, int *stack){
 
+	// Switch case that executes the math functions depending on the OP code.
 	switch(env->ir.op){
 		case 10: //NEG
 			env->R[env->ir.r] = -env->R[env->ir.r];
@@ -289,12 +313,14 @@ void printStack(int printValue,enviroment *env,int sp,int bp,int* stack,int l,FI
 	 	 
 	switch(printValue){
 		
-		case 1:			
+		case 1:		
+			// Prints the first 8 outputs of stack trace to an output file.
 			fprintf(ofp,"%d %s %d %d %d    [%d %d %d]    ",env->pcPrev,opCode[env->ir.op],env->ir.r,env->ir.l,
 						env->ir.m, env->pc, env->bp, env->sp);
 						
 			break;
 		case 2:
+			// Recursively prints the contents of the stack to an iput file.
 			if (bp == 1) {
 				if (l > 0) {
 					fprintf(ofp,"|");
@@ -311,6 +337,7 @@ void printStack(int printValue,enviroment *env,int sp,int bp,int* stack,int l,FI
 			}
 			break;
 		case 3:
+			// Points the registers to an output file.
 			fprintf(ofp,"\n\tRegisters:[");
 			
 			for(i=0;i<8;i++){
